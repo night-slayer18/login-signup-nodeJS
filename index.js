fb821f51e5e8397977831af93c18a423c058e51d
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2023-09-09 23:22:43 +0530

    added the routes

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(cors());
 
 //routes
-console.log('Login: /api/auth');
-console.log('Signup: /api/users');
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
